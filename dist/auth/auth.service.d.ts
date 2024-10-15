e20bbf930a9cca261202a469067c99e8ca0fde07
Eduardo Batista <eduardo_batistaalves@hotmail.com>
2024-10-15 14:45:29 -0300

    remove dist

--- dist/auth/auth.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { JwtService } from "@nestjs/jwt";
-import { UsersService } from "src/app/users/users.service";
-export declare class AuthService {
-    private userService;
-    private jwtService;
-    constructor(userService: UsersService, jwtService: JwtService);
-    signIn(email: string): Promise<any>;
-}

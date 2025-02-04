15ba06988f7e4783c4dab77f157b410f0f3cc069
Eduardo Batista <eduardo_batistaalves@hotmail.com>
2025-02-04 09:32:28 -0300

    feat: return more info in jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -14,6 +14,10 @@ export class AuthService {
     };
     return {
       access_token: this.jwtService.sign(payload),
+      id: user.id,
+      email: user.email,
+      role: user.userRole,
+      status: user.status,
     };
   }
 }
